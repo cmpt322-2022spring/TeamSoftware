6faa1463b81941e5e868716714227b3a8b2ee0df
William Weinberger <wlw0818@westminstercollege.edu>
2022-02-26 16:53:41 -0700

    Added player movement to and from chaser and losing.

--- Teamsoftware/Assets/Scripts/LevelManager.cs
+++ Teamsoftware/Assets/Scripts/LevelManager.cs
@@ -12,7 +12,7 @@ public class LevelManager : MonoBehaviour
     public GameObject correctAnswerPanel;
 
     // Background info
-    public float backrgoundSpeed;
+    public float backgroundSpeed;
     public Renderer backgroundRenderer;
 
     // panel actvation time and timer
@@ -28,10 +28,14 @@ public class LevelManager : MonoBehaviour
 
     // Win and lose screens
     public GameObject winScreen;
+    public GameObject loseScreen;
 
     // Player Movement
     public Transform player;
+    public float moveSpeed = 10f;
+    [Range(0, 4)]
     public int actualPos = 3;
+    [Range(0, 4)]
     public int newPos = 3;
     public List<Transform> positions = new List<Transform>();
 
@@ -39,6 +43,7 @@ public class LevelManager : MonoBehaviour
     {
         questionPanel.SetActive(false);
         winScreen.SetActive(false);
+        loseScreen.SetActive(false);
         ShowQuestion();
         questions = FindObjectOfType<QuestionData>().questions;
     }
@@ -66,16 +71,34 @@ public class LevelManager : MonoBehaviour
         // ... Move to the new pos
         if (actualPos != newPos)
         {
-            // player.position = Vector3.MoveTowards(player)
+            player.position = Vector3.MoveTowards(player.position,
+                new Vector3(positions[newPos].position.x, player.position.y, player.position.z),
+                moveSpeed * Time.deltaTime);
+            if (player.position.x == positions[newPos].position.x)
+            {
+                actualPos = newPos;
+            }
+        }
+
+        if (newPos == 0 && !loseScreen.activeSelf)
+        {
+            loseScreen.SetActive(true);
+            correctAnswerPanel.GetComponentInChildren<Button>().interactable = false;
         }
 
     }
 
+    /// <summary>
+    /// Moves the background by background speed
+    /// </summary>
     void MoveBackground()
     {
-        backgroundRenderer.material.mainTextureOffset += new Vector2(backrgoundSpeed * Time.deltaTime, 0);
+        backgroundRenderer.material.mainTextureOffset += new Vector2(backgroundSpeed * Time.deltaTime, 0);
     }
 
+    /// <summary>
+    /// Sets a correct question answer panel to off and shows the next question
+    /// </summary>
     public void ShowQuestion()
     {
         correctAnswerPanel.SetActive(false);
@@ -92,18 +115,23 @@ public class LevelManager : MonoBehaviour
         
     }
 
+    /// <summary>
+    /// Shows the answer panel and tells whether it was correct or not.
+    /// </summary>
+    /// <param name="correctAnswer">A boolean describing whether the answer was correct or not.</param>
     public void ShowAnswerPanel(bool correctAnswer)
     {
         correctAnswerPanel.SetActive(true);
         if (correctAnswer)
         {
+            ChangePos(1);
             Text answerTitle = GameObject.Find("AnswerTitle").GetComponent<Text>();
             answerTitle.text = "Correct!";
             answerTitle.color = Color.green;
-
         }
         else
         {
+            ChangePos(-1);
             Text answerTitle = GameObject.Find("AnswerTitle").GetComponent<Text>();
             answerTitle.text = "Incorrect!";
             answerTitle.color = Color.red;
@@ -127,14 +155,36 @@ public class LevelManager : MonoBehaviour
 
     }
 
+    /// <summary>
+    /// Restarts the level
+    /// NOTE: Probably a test function, we can potentially modify later
+    /// </summary>
     public void Restart()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
+    /// <summary>
+    /// Changes the new pos of the player
+    /// </summary>
+    /// <param name="_pos">Pos to change the newPos by (-1 or +1 ONLY)</param>
     public void ChangePos(int _pos)
     {
-        newPos += _pos;
+        if (_pos == -1 || _pos == 1)
+        {
+            if (newPos < 4 && _pos == 1)
+            {
+                newPos += _pos;
+            }
+            if (newPos > 0 && _pos == -1)
+            {
+                newPos += _pos;
+            }
+        }
+        else
+        {
+            print("ERROR! _pos must be +1 or -1 ONLY!");
+        }
     }
 
 }
