829020cfee5df6b9b056af5a0afb5c7d2b17be50
MikkelSol <mes1002@westminstercollege.edu>
2022-03-02 22:33:52 -0700

    new scripts and register scene
    Setting up the database with php

---
+++ Teamsoftware/Assets/Scripts/register.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+public class register : MonoBehaviour
+{
+    public InputField nameField;
+    public InputField passwordField;
+    
+    public Button submitButton;
+
+    public void CallRegister() {
+        StartCoroutine(Register());
+
+    }
+
+    //Might have to use WWW instead, but this is the "new" class to connect.
+    IEnumerator Register() {
+        //Adds form 
+        WWWForm form = new WWWForm();
+        form.AddField("name", nameField.text);
+        form.AddField("password", passwordField.text);
+        
+        //Post 
+        UnityWebRequest www = UnityWebRequest.Post("http://localhost/sqlconnect/register.php", form);
+        yield return www.SendWebRequest();
+
+        //sends error messages, if not loads logininScene again.
+        if(www.result == UnityWebRequest.Result.Success) {
+            Debug.Log("User created successfully.");
+            UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+        }
+        else {
+            //Might have to play around with www.error (text or some other stuff)
+            Debug.Log("User creation failed. Error #" + www.error);
+        }
+    }
+
+    public void VerifyInputs(){
+        submitButton.interactable = (nameField.text.Length >= 4 && passwordField.text.Length >=4);
+    }
+
+}
