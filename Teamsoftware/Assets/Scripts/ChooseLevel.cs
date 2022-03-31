c29195d12e41af63e2534e764fc172358cbdd1b9
Shawn <1364321824@qq.com>
2022-03-31 11:07:28 -0600

    Background and New Pages

---
+++ Teamsoftware/Assets/Scripts/ChooseLevel.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ChooseLevel : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject levelPage;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Level()
+    {
+        levelPage.GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
+    }
+    public void BackToMainMenu()
+    {
+        levelPage.GetComponent<RectTransform>().anchoredPosition = new Vector2(500f, 500f);
+    }
+}
