811fa48cbd829ea15779e9d1e27fb0144e7b7747
Shawn <1364321824@qq.com>
2022-04-05 11:30:20 -0600

    New changes

--- Teamsoftware/Assets/Scripts/TeacherLoginPage.cs
+++ Teamsoftware/Assets/Scripts/TeacherLoginPage.cs
@@ -6,6 +6,9 @@ using UnityEngine.UI;
 public class TeacherLoginPage : MonoBehaviour
 {
     public GameObject teacherSettingPage;
+    public GameObject problemEditingPage;
+    public GameObject removeStudentsPage;
+    public GameObject TeacherMainMenuPage;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,8 +25,30 @@ public class TeacherLoginPage : MonoBehaviour
     {
         teacherSettingPage.GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
     }
+    public void ShowProblemPage()
+    {
+        problemEditingPage.GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
+        teacherSettingPage.GetComponent<RectTransform>().anchoredPosition = new Vector2(500f, 500f);
+        removeStudentsPage.GetComponent<RectTransform>().anchoredPosition = new Vector2(500f, 500f);
+        TeacherMainMenuPage.GetComponent<RectTransform>().anchoredPosition = new Vector2(500f, 500f);
+
+    }
+    public void ShowStudentRemovePage()
+    {
+        removeStudentsPage.GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
+    }
     public void BackToTeacherMain()
     {
         teacherSettingPage.GetComponent<RectTransform>().anchoredPosition = new Vector2(500f, 500f);
+        TeacherMainMenuPage.GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
+    }
+    public void EditingtoSetting()
+    {
+        problemEditingPage.GetComponent<RectTransform>().anchoredPosition = new Vector2(500f, 500f);
+        teacherSettingPage.GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
+    }
+    public void StudentstoSetting()
+    {
+        removeStudentsPage.GetComponent<RectTransform>().anchoredPosition = new Vector2(500f, 500f);
     }
 }
